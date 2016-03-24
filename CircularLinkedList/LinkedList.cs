cf3d1c309d2fa5301118b9736fc91d5cae4c3ce7
C S <sethuchitra@live.com>
2016-03-24 13:58:04 -0700

    Linked List - Circular

---
+++ CircularLinkedList/LinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularLinkedList
+{
+    //1,3,2,3
+    public class LinkedList
+    {
+        public Node head;
+        public int Circular(Node head)
+        {
+
+            Node slow = head;
+            Node fast = head;
+            while (slow != null && fast != null && fast.next != null)
+            {
+                //Move slow pointer one step and fast pointer two steps
+                slow = slow.next;
+                fast = fast.next.next;
+                //Find where slow and fast pointer meets
+                if (slow == fast)
+                    break;
+            }
+            //If there is no meeting point then exit
+            if (fast == null || fast.next == null)
+                return -1;
+            //Move slow pointer to head and keep fast at the meeting point 
+            slow = head;
+             while(slow!=fast)
+            {
+                /***Move slow pointer and fast pointer at speed of one step. 
+                If they both move at the same rate then they will meet at loop start***/
+                slow = slow.next;
+                fast = fast.next;
+             }
+            //return the new meeting point
+            return fast.data;
+            }
+            
+        
+
+        public void PrintList()
+        {
+            Node n = head;
+            while(n!=null)
+            {
+                Console.WriteLine(n.data + "->");
+                n = n.next;
+            }
+        }
+    }
+}
---
+++ CircularLinkedList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularLinkedList
+{
+    //Write an algorithm to determine if a linkedlist is circular. FOLLOW UP: Determine where the circle meets. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.head = new Node(1);
+            Node second = new Node(3);
+            Node third = new Node(2);
+            Node four = new Node(3);
+            list.head.next = second;
+            list.head.next.next = third;
+            list.head.next.next.next = four;
+            four.next = list.head.next;
+           
+
+
+            Console.WriteLine(list.Circular(list.head)); 
+            //list.PrintList();
+        }
+    }
+}
